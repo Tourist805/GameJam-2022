e5ff9e3f14d82cbed3abb39d41914e9a2085a12c
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-04-23 16:36:44 +0100

    Scene Skybox update

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
     [SerializeField] private Vector3 _rot;
+    [SerializeField] private GameObject _target;
 
 
     private void Update() {
         transform.Rotate(_rot * Time.deltaTime);
+        //transform.RotateAround(_target.transform.position, Vector3.up, 20 * Time.deltaTime);
     }
 }
